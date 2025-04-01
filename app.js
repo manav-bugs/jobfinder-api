1f1454a6b115227d7d78f04354febbc95141cc3b
manav-bugs <manavmodi1102@gmail.com>
2025-04-01 23:48:28 +0530

    change

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ require('dotenv').config({ path: './config/.env' });
 
 // cors setup for establishing connection b/w frontend and backend
 app.use(cors({
-    origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
+    origin: ['http://127.0.0.1:5173', 'http://localhost:5173',"https://jobfinder-frontend-six.vercel.app"],
     methods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'],
     credentials: true
 }));
